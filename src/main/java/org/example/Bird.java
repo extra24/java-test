b1dcd35047df69da15be54af1c59ef0d5c39d186
aram <6aram@naver.com>
2025-07-11 15:04:31 +0900

    다중 인터페이스 구현 예제

--- src/main/java/org/example/Bird.java
+++ src/main/java/org/example/Bird.java
@@ -1,14 +1,22 @@
 package org.example;
 
-public class Bird implements Flyable{
+public class Bird implements Flyable, Walkable{
 
     @Override
     public void fly() {
         System.out.println("새가 날아갑니다.");
     }
 
+    @Override
+    public void walk() {
+        System.out.println("새가 걷습니다.");
+    }
+
     public static void main(String[] args) {
         Bird bird = new Bird();
         bird.fly();
+        bird.walk();
     }
+
+
 }
