4185fa55fa6b20977bdffa097c563cca2e17cc69
aram <6aram@naver.com>
2025-07-11 14:31:04 +0900

    Car 생성자

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -43,12 +43,33 @@ public class Car {
         return this.year;
     }
 
+    // 생성자 : 반환형이 없다. -> 기본으로 생성자는 자동으로 생성되어있다.
+    // 가장 먼저 생성됨
+    public Car(){
+        // 기본 생성자
+        System.out.println("자동차 객체가 생성되었습니다.");
+    }
+
+    //속성들을 받아내는 생성자 -> 이걸 가져와서 아래에서 사용할 수 있다.
+    public Car(String brand, String model, int year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    //변수를 일부만 넣어서 만들 수 있는 생성자도 있다.
+    public Car(String brand, String model){
+        this.brand = brand;
+        this.model = model;
+    }
+
     //객체
     public static void main(String[] args){
-        Car myCar = new Car(); // 객체 만들기
-        myCar.setBrand("기아"); //여기서 brand 값을 넣는다.
-        myCar.setModel("K9"); // 값 생성(데이터 삽입)
-        myCar.setYear(2012);
+//        Car myCar = new Car(); // 객체 만들기
+        Car myCar = new Car("현대", "k9", 2012);
+//        myCar.setBrand("기아"); //여기서 brand 값을 넣는다.
+//        myCar.setModel("K9"); // 값 생성(데이터 삽입)
+//        myCar.setYear(2012);
 
         System.out.println("브랜드 : "+ myCar.getBrand());
         System.out.println("모델 : "+ myCar.getModel());
