03d7f01ce233a6e31349e533d7668809ef949999
aram <6aram@naver.com>
2025-07-11 13:58:24 +0900

    계산기 getter, setter 적용 및 오타 수정

--- src/main/java/org/example/CalCalculate.java
+++ src/main/java/org/example/CalCalculate.java
@@ -2,40 +2,62 @@ package org.example;
 
 public class CalCalculate {
 
+    //git commit -m "feat: 작업내용"
+    //git push origin feature/calculate
+
+    //git hub 화면에서 Pull Request 생성
+    //feature/calculate 브랜치 넣고 PR에서 강사님 reviewer로 넣기
+
+    //리뷰가 끝나면 merge 버튼 누르기
+
+    private int a;
+    private int b;
+
     //덧셈
     public int add(int a, int b){
-        int answer = a + b;
-        return answer;
+        return a + b;
     }
 
     //뺄셈
     public int minus(int a, int b){
-        int answer = a - b;
-        return answer;
+        return a - b;
     }
 
     //나눗셈
     public double divide(int a, int b){
-        double answer = a / b;
-        return answer;
+        return (double) a / b;
     }
 
     //곱셈
-    public int multiplely(int a, int b){
-        int answer = a * b;
-        return answer;
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public void setA(int a){
+        this.a = a;
+    }
+
+    public void setB(int b){
+        this.b = b;
+    }
+
+    public int getA(){
+        return this.a;
+    }
+
+    public int getB(){
+        return this.b;
     }
 
     //main
     public static void main(String[] args){
         CalCalculate myCalc = new CalCalculate();
-        System.out.println(myCalc.add(6, 3));
-        System.out.println(myCalc.minus(6, 3));
-        System.out.println(myCalc.divide(6, 3));
-        System.out.println(myCalc.multiplely(6, 3));
-    }
+        myCalc.setA(6);
+        myCalc.setB(3);
 
-    //feature 브랜치 넣고 PR에서 강사님 reviewer로 넣기
-    //git commit -m "feat: 작업내용"
-    //git push origin feature/calculate
+        System.out.println("덧셈 : " + myCalc.add(myCalc.getA(), myCalc.getB()));
+        System.out.println("뺄셈 : " + myCalc.minus(myCalc.getA(), myCalc.getB()));
+        System.out.println("나눗셈 : " + myCalc.divide(myCalc.getA(), myCalc.getB()));
+        System.out.println("곱셈 : " + myCalc.multiply(myCalc.getA(), myCalc.getB()));
+    }
 }
